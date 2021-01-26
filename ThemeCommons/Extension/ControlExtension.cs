ab8c04d6260a95cc879e3cf4255266bd650c329a
Jens Krumsieck <jenso666@live.de>
2021-01-26 15:00:28 +0100

    Button

---
+++ ThemeCommons/Extension/ControlExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace ThemeCommons.Extension
+{
+    public static class ControlExtension
+    {
+        /// <summary>
+        /// Attached SpecialColorProperty
+        /// </summary>
+        public static readonly DependencyProperty SpecialColorProperty =
+            DependencyProperty.RegisterAttached("SpecialColor", typeof(Color), typeof(ControlExtension),
+                new PropertyMetadata(Colors.OrangeRed));
+        public static void SetSpecialColor(DependencyObject obj, Color value) => 
+            obj.SetValue(SpecialColorProperty, value);
+        public static Color GetSpecialColor(DependencyObject obj) => 
+            (Color)obj.GetValue(SpecialColorProperty);
+    }
+}
