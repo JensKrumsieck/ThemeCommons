355957ca389e94a602c702d44017677702b231d8
Jens Krumsieck <jenso666@live.de>
2021-03-20 12:24:33 +0100

    fix minwidth/height behaviour

--- ThemeCommons/Controls/DefaultWindow.cs
+++ ThemeCommons/Controls/DefaultWindow.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Interop;
 using System.Windows.Media;
@@ -61,11 +62,33 @@ namespace ThemeCommons.Controls
             NativeMethods.EnableBlur(handle);
         }
 
-        private static IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        private IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
         {
-            if (msg != 0x0024) return IntPtr.Zero;
-            NativeMethods.WMGetMinMaxInfo(hwnd, lParam);
-            handled = true;
+            switch (msg)
+            {
+                case 0x0024:
+                    NativeMethods.WMGetMinMaxInfo(hwnd, lParam);
+                    handled = true;
+                    break;
+                case 0x0046:
+                    var pos = (WINDOWPOS)Marshal.PtrToStructure(lParam, typeof(WINDOWPOS));
+                    var src = PresentationSource.FromVisual(this);
+                    if ((pos.flags & 0x0002) != 0) return IntPtr.Zero;
+                    var wnd = (Window)HwndSource.FromHwnd(hwnd)?.RootVisual;
+                    if (wnd == null || src == null) return IntPtr.Zero;
+
+                    var dpiX = src.CompositionTarget?.TransformToDevice.M11 ?? 1;
+                    var dpiY = src.CompositionTarget?.TransformToDevice.M22 ?? 1;
+
+                    var changed = false;
+                    if (pos.cx / dpiX < MinWidth) { pos.cx = (int)(MinWidth * dpiX); changed = true; }
+                    if (pos.cy / dpiY < MinHeight) { pos.cy = (int)(MinHeight * dpiY); changed = true; }
+                    if (!changed) return IntPtr.Zero;
+
+                    Marshal.StructureToPtr(pos, lParam, true);
+                    handled = true;
+                    break;
+            }
             return IntPtr.Zero;
         }
     }
---
+++ ThemeCommons/Extension/Native/WindowPos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+// ReSharper disable once InconsistentNaming
+namespace ThemeCommons.Extension.Native
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WINDOWPOS
+    {
+        public IntPtr hwnd;
+        public IntPtr hwndInsertAfter;
+        public int x;
+        public int y;
+        public int cx;
+        public int cy;
+        public int flags;
+    }
+}
